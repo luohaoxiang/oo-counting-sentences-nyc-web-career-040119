0bfc22557c72b0aa61d7b7dabcb8cfe0f58f588b
Adam Robinson <luohaoxiang@yahoo.com>
2019-04-08 12:17:07 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,33 @@ require 'pry'
 
 class String
 
+
   def sentence?
-    
+    if self.slice(-1) == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.slice(-1) == "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.slice(-1) == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
 
   end
+
 end
